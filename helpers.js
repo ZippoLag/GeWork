0e5ca01ce868a330800046b8de172156d1839154
Sebastian Vansteenkiste <sebavanten@hotmail.com>
2019-06-18 14:37:57 -0300

    Mostrando lista de estados WiP -TODO: agregar estilos y disponibilidades

--- helpers.js
+++ helpers.js
@@ -1,6 +1,68 @@
 var claves = ['nombreUsuario', 'paginaAnterior', 'pais', 'provincia', 'localidad', 'fecha', 'turno'];
 var globales = {};
 
+var coworks = {
+    "utn":{
+        "nombre":"CoWork Uteniano",
+        "direccion":"Zeballos 14XX",
+        "url":"http://google.com/",
+        "espacios":[
+            {
+                "nombre":"Sala Java",
+                "precioMedia":20,
+                "precioCompleta":35,
+                "ubicacion":"5º piso, ala única, junto a los baños.",
+                "prestaciones":[
+                    {
+                        "nombre":"Wi-Fi",
+                        "descripcion":"Conexión inalámbrica segura",
+                        "icono":"fa-wifi"
+                    },
+                    {
+                        "nombre":"proyector",
+                        "descripcion":"Proyector y pantalla", //TODO: encontrar ícono de verdad
+                        "icono":"fa-eye"
+                    }
+                ],
+                "puestos":[
+                    {
+                        "id": 0,
+                        "ubicacion":"Rincón noroeste"
+                    }
+                ]
+            },
+            {
+                "nombre":"Sala .Net",
+                "precioMedia":30,
+                "precioCompleta":50,
+                "ubicacion":"5º piso, ala única, junto al ascensor",
+                "prestaciones":[
+                    {
+                        "nombre":"Wi-Fi",
+                        "descripcion":"Conexión inalámbrica segura",
+                        "icono":"fa-wifi"
+                    },
+                    {
+                        "nombre":"pizarron",
+                        "descripcion":"Pizarrón",
+                        "icono":"fa-chalkboard"
+                    }
+                ],
+                "puestos":[
+                    {
+                        "id": 0,
+                        "ubicacion":"Junto a la entrada"
+                    },
+                    {
+                        "id": 1,
+                        "ubicacion":"Junto al escritorio frontal"
+                    }
+                ]
+            }
+        ]
+    }
+};
+
 /**
 * @param String name
 * @return null

45061f325072a8126c09ec52f99fc5bdfe6d3ba0
Cindi <cindimartin23@hotmail.com>
2019-11-01 12:08:11 -0300

    Agregada auditoria.

--- espacios/admin.py
+++ espacios/admin.py
@@ -5,12 +5,12 @@ admin.site.site_header = "GeWork Admin"
 admin.site.site_title = "GeWork Admin Area"
 admin.site.index_title = "Bienvenido a GeWork admin area"
 
-admin.site.register(MyModel)
+#admin.site.register(MyModel)
 class MyModelAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         if obj:
             return ["created_date", "created_by", "modified_at", "modified_by"]
-        return ["created_date",]
+        return ["created_date", "created_by", "modified_at", "modified_by"]
 
     def save_model(self, request, obj, form, change):
         obj.created_by = request.user

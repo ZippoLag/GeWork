e55f48ee29d4a3b71834a9016d27c1b234eddce7
Cindi <cindimartin23@hotmail.com>
2019-11-04 19:13:09 -0300

    Agregado codigo para devolver listas y objetos unicos de las clases Cowork, Espacio, Prestacion
    Tambien se agregaron archivos html a la carpeta frontend_bundle para probar los resultados de la busqueda de objetos únicos

--- espacios/serializers.py
+++ espacios/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import Espacio
+from .models import Espacio, Prestacion, Cowork
 
 # TODO: crear serializers para servir los modelos mediante la API REST:
 
 class EspacioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Espacio
+        fields = '__all__'
+
+class PrestacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prestacion
+        fields = '__all__'
+
+class CoworkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cowork
         fields = '__all__'
--- espacios/views.py
+++ espacios/views.py
@@ -3,16 +3,18 @@ import json
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
 from rest_framework import generics
-from .models import Espacio
-from .serializers import EspacioSerializer
+from .models import Espacio, Prestacion, Cowork
+from .serializers import EspacioSerializer, PrestacionSerializer, CoworkSerializer
+from django.shortcuts import get_object_or_404, render
 
 # TODO: crear vistas para servir los objetos serializados (y demás)
 
+# Devuelve lista de Espacios
 class EspacioListCreate(generics.ListCreateAPIView):
     queryset = Espacio.objects.all()
     serializer_class = EspacioSerializer
 
-
+# Devuelve detalles de Usuario Logueado
 def get_detalles_usuario(request):
     # TODO: obtener la instancia de Usuario (o Perfil, o como le llamemos) relacionada al usuario autenticado por django y enviar _sólo_ los datos que necesitemos en el frontend
     response = JsonResponse({
@@ -20,3 +22,37 @@ def get_detalles_usuario(request):
     })
 
     return response
+
+# Devuelve un Espacio
+def EspacioDetail(request, id):
+    try:
+        espacio = Espacio.objects.get(pk=id)
+    except Espacio.DoesNotExist:
+        raise Http404("Espacio does not exist")
+    return render(request, 'frontend_bundle/detail-espacio.html', { 'espacio': espacio })
+
+# Devuelve lista de Prestaciones
+class PrestacionListCreate(generics.ListCreateAPIView):
+    queryset = Prestacion.objects.all()
+    serializer_class = PrestacionSerializer
+
+# Devuelve una Prestacion
+def PrestacionDetail(request, id):
+    try:
+        prestacion = Prestacion.objects.get(pk=id)
+    except Prestacion.DoesNotExist:
+        raise Http404("Prestacion does not exist")
+    return render(request, 'frontend_bundle/detail-prestacion.html', { 'prestacion': prestacion })
+
+# Devuelve lista de Coworks
+class CoworkListCreate(generics.ListCreateAPIView):
+    queryset = Cowork.objects.all()
+    serializer_class = CoworkSerializer
+
+# Devuelve un Cowork
+def CoworkDetail(request, id):
+    try:
+        cowork = Cowork.objects.get(pk=id)
+    except Cowork.DoesNotExist:
+        raise Http404("Cowork does not exist")
+    return render(request, 'frontend_bundle/detail-cowork.html', { 'cowork': cowork })

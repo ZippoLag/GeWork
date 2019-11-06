becbd51355d61b4a81384d22ab57c68e3e9d814f
Cindi <cindimartin23@hotmail.com>
2019-11-06 09:56:53 -0300

    Agregado código a admin.py para grabar usuario en created_by y modified_by.
    No está funcionando.

--- espacios/admin.py
+++ espacios/admin.py
@@ -5,14 +5,16 @@ admin.site.site_header = "GeWork Admin"
 admin.site.site_title = "GeWork Admin Area"
 admin.site.index_title = "Bienvenido a GeWork admin area"
 
-#admin.site.register(MyModel)
+#@admin.register(MyModel)
 class MyModelAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         if obj:
             return ["created_date", "created_by", "modified_at", "modified_by"]
-        return ["created_date", "created_by", "modified_at", "modified_by"]
+        return ["created_date",]
 
+    # TODO: no está funcionando. Por que no?
     def save_model(self, request, obj, form, change):
-        obj.created_by = request.user
-        # TODO: detectar si estamos editando o creando y actualizar concordantemente
-        super().save_model(request, obj, form, change)
+        if not obj.created_by:
+            obj.created_by = request.user.username
+        obj.modified_by = request.user.username
+        super(MyModelAdmin, self).save_model(request, obj, form, change)

0a67edac85bc9bd1d2a349ae082cca499b159f1b
Sebastian Vansteenkiste <sebavanten@hotmail.com>
2019-11-09 20:05:15 -0300

    Todos los botones en pantalla de reserva

--- frontend/src/components/iniciarReserva/IniciarReserva.js
+++ frontend/src/components/iniciarReserva/IniciarReserva.js
@@ -16,14 +16,24 @@ export class IniciarReserva extends Component {
 
   render() {
     return (
-      <Row className="d-flex justify-content-around align-items-center">
-        <Col className="col-10 col-md-4 pt-5">
-          <Link className="w-100 btn btn-primary" to="/reservar-puesto">Reservar Puesto</Link>
-        </Col>
-        <Col className="col-10 col-md-4 pt-5">
-          <div className="w-100 btn btn-primary btn-disabled" disabled>Reservar Sala</div>
-        </Col>
-      </Row>
+      <div>
+        <Row className="d-flex justify-content-around align-items-center">
+          <Col className="col-10 col-md-4 pt-5">
+            <Link className="w-100 btn btn-primary" to="/reservar-puesto">Reservar Puesto</Link>
+          </Col>
+          <Col className="col-10 col-md-4 pt-5">
+            <div className="w-100 btn btn-primary btn-disabled" disabled>Reservar Sala</div>
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-around align-items-center">
+          <Col className="col-10 col-md-4 pt-5">
+            <Link className="w-100 btn btn-primary btn-disabled" disabled>Ofrecer Puesto</Link>
+          </Col>
+          <Col className="col-10 col-md-4 pt-5">
+            <div className="w-100 btn btn-primary btn-disabled" disabled>Ofrecer Sala</div>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }

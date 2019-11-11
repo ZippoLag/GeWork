95253e16f6a35a29c83183612010421daef960c2
Sebastian Vansteenkiste <sebavanten@hotmail.com>
2019-11-10 21:32:32 -0300

    Obteniendo localidades et al

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,25 +15,59 @@ import './index.css';
 
 class Index extends Component {
   state = {
-    usuario: {}
+    usuario: {},
+    espacios: [],
+    localidades: [],
+    provincias: [],
+    paises: []
   }
 
-  componentDidMount(){
-    let instance = this;
+  getInformacionGeografica(){
+    this.getPaises();
+  }
 
+  getPaises(){
+    httpClient.get(`api/paises/`)
+    .then((data) => this.setState({paises: data}))
+    .then(this.getProvincias())
+    .catch((error) => console.log(error));
+  }
+
+  getProvincias(){
+    httpClient.get(`api/provincias/`)
+    .then((data) => this.setState({provincias: data.map(provincia => this.completarProvincia(provincia))}))
+    .then(this.getLocalidades())
+    .catch((error) => console.log(error));
+  }
+
+  getLocalidades(){
+    httpClient.get(`api/localidades/`)
+    .then((data) => this.setState({localidades: data.map(localidad => this.completarLocalidad(localidad))}))
+    .catch((error) => console.log(error));
+  }
+
+  completarProvincia = (provincia) => {
+    provincia.pais = this.state.paises.find(pais => pais.id_pais === provincia.pais) || provincia.pais;
+
+    return provincia;
+  };
+
+  completarLocalidad = (localidad) => {
+    localidad.provincia = this.state.paises.find(provincia => provincia.id_provincia === localidad.provincia) || localidad.provincia;
+
+    return localidad;
+  };
+
+  componentDidMount(){
     //Si hay un token de autenticación en las cookies del navegador, significa que ya inició sesión un usuario, por lo que podemos obtener sus detalles desde el backend
     if(getCookie('csrftoken')){
       httpClient
-        .get(`api/get_detalles_usuario`)
-        .then((data) => {
-          instance.setState({
-            usuario: data
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+        .get(`api/get_detalles_usuario/`)
+        .then((data) => this.setState({usuario: data}))
+        .catch((error) => console.log(error));
     }
+
+    this.getInformacionGeografica();
   }
 
     render() {

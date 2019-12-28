505e2e58897107bd0b390b6ee5c76aa9e1123acd
Sebastian Vansteenkiste <sebavanten@hotmail.com>
2019-12-28 18:31:19 -0300

    Autoformateo con Prettier

--- frontend/src/components/selectorUbicacion/SelectorUbicacion.js
+++ frontend/src/components/selectorUbicacion/SelectorUbicacion.js
@@ -25,18 +25,19 @@ export class SelectorUbicacion extends Component {
   render() {
     //TODO: mejorar apariencia! (ver por qué no andaban los dropdowns de bootstrap!)
     return (
-      <Row className="d-flex justify-content-around align-items-center">
-        <Col className="form-group col-12 col-md-3" role="group" >
-          <label htmlFor="pais-select">País:</label>
+      <Row className='d-flex justify-content-around align-items-center'>
+        <Col className='form-group col-12 col-md-3' role='group'>
+          <label htmlFor='pais-select'>País:</label>
           {/* TODO: onSelect debería actualizar y limpiar el selector de provincias (y localidades, y limpiar mapa?) */}
-          <select className="from-control" id="pais-select" >
-            {this.props.paises.map(pais => (
+          <select className='from-control' id='pais-select'>
+            {this.props.paises.map((pais) => (
               <option
-              key={pais.id_pais}
-              active={
-                (this.state.id_pais === pais.id_pais ||
-                (this.state.id_pais === 0 && this.props.id_pais_default === pais.id_pais)).toString()
-              }
+                key={pais.id_pais}
+                active={(
+                  this.state.id_pais === pais.id_pais ||
+                  (this.state.id_pais === 0 &&
+                    this.props.id_pais_default === pais.id_pais)
+                ).toString()}
               >
                 {pais.nombre_pais}
               </option>
@@ -44,17 +45,18 @@ export class SelectorUbicacion extends Component {
           </select>
         </Col>
 
-        <Col className="form-group col-12 col-md-3" role="group" >
-          <label htmlFor="provincia-select">Provincia:</label>
+        <Col className='form-group col-12 col-md-3' role='group'>
+          <label htmlFor='provincia-select'>Provincia:</label>
           {/* TODO: onSelect debería actualizar y limpiar el selector de localidades (y limpiar mapa?) */}
-          <select className="from-control" id="provincia-select" >
-            {this.props.provincias.map(provincia => (
+          <select className='from-control' id='provincia-select'>
+            {this.props.provincias.map((provincia) => (
               <option
-              key={provincia.id_provincia}
-              active={
-                (this.state.id_provincia === provincia.id_provincia ||
-                (this.state.id_provincia === 0 && this.props.id_provincia_default === provincia.id_provincia)).toString()
-              }
+                key={provincia.id_provincia}
+                active={(
+                  this.state.id_provincia === provincia.id_provincia ||
+                  (this.state.id_provincia === 0 &&
+                    this.props.id_provincia_default === provincia.id_provincia)
+                ).toString()}
               >
                 {provincia.nombre_provincia}
               </option>
@@ -62,17 +64,18 @@ export class SelectorUbicacion extends Component {
           </select>
         </Col>
 
-        <Col className="form-group col-12 col-md-3" role="group" >
-          <label htmlFor="localidad-select">Localidad:</label>
+        <Col className='form-group col-12 col-md-3' role='group'>
+          <label htmlFor='localidad-select'>Localidad:</label>
           {/* TODO: onSelect debería actualizar y limpiar el mapa */}
-          <select className="from-control" id="localidad-select" >
-            {this.props.localidades.map(localidad => (
+          <select className='from-control' id='localidad-select'>
+            {this.props.localidades.map((localidad) => (
               <option
-              key={localidad.id_localidad}
-              active={
-                (this.state.id_localidad === localidad.id_localidad ||
-                (this.state.id_localidad === 0 && this.props.id_localidad_default === localidad.id_localidad)).toString()
-              }
+                key={localidad.id_localidad}
+                active={(
+                  this.state.id_localidad === localidad.id_localidad ||
+                  (this.state.id_localidad === 0 &&
+                    this.props.id_localidad_default === localidad.id_localidad)
+                ).toString()}
               >
                 {localidad.nombre_localidad}
               </option>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
-import React, { Suspense , Component} from 'react';
+import React, { Suspense, Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-import Cabecera from './components/cabecera/Cabecera'
-import Pie from './components/pie/Pie'
+import Cabecera from './components/cabecera/Cabecera';
+import Pie from './components/pie/Pie';
 
 import ElegirAccion from './components/elegirAccion/ElegirAccion';
 import ReservarPuesto from './components/reservarPuesto/ReservarPuesto';
@@ -20,114 +20,135 @@ class Index extends Component {
     localidades: [],
     provincias: [],
     paises: []
-  }
+  };
 
-  getLocalidadGeo(){
+  getLocalidadGeo() {
     let localidad;
 
-    if (this.state.localidades.length > 0){
+    if (this.state.localidades.length > 0) {
       //TODO: usar API de ubicación para obtener localidad más cercana a la actual como default
       localidad = this.state.localidades[0];
-      if(Number.isInteger(localidad.provincia)){
+      if (Number.isInteger(localidad.provincia)) {
         localidad.provincia = {
           id_provincia: localidad.provincia,
-          nombre_provincia: "Cargando...",
-          pais:{
+          nombre_provincia: 'Cargando...',
+          pais: {
             id_pais: 0,
-            nombre_pais: "Cargando..."
+            nombre_pais: 'Cargando...'
           }
         };
       }
     } else {
       localidad = {
-        id_localidad:0,
+        id_localidad: 0,
         nombre_localidad: 'Cargando...',
-        provincia:{
-          id_provincia:0,
+        provincia: {
+          id_provincia: 0,
           nombre_provincia: 'Cargando...',
-          pais:{
+          pais: {
             id_pais: 0,
             nombre_pais: 'Cargando...'
           }
         }
-      }
+      };
     }
 
     return localidad;
   }
 
-  fetchInformacionGeografica(){
+  fetchInformacionGeografica() {
     this.fetchPaises();
   }
 
-  fetchPaises(){
-    httpClient.get(`api/paises/`)
-    .then((data) => this.setState({paises: data}))
-    .then(this.fetchProvincias())
-    .catch((error) => console.log(error));
+  fetchPaises() {
+    httpClient
+      .get(`api/paises/`)
+      .then((data) => this.setState({ paises: data }))
+      .then(this.fetchProvincias())
+      .catch((error) => console.log(error));
   }
 
-  fetchProvincias(){
-    httpClient.get(`api/provincias/`)
-    .then((data) => this.setState({provincias: data.map(provincia => this.completarProvincia(provincia))}))
-    .then(this.fetchLocalidades())
-    .catch((error) => console.log(error));
+  fetchProvincias() {
+    httpClient
+      .get(`api/provincias/`)
+      .then((data) =>
+        this.setState({
+          provincias: data.map((provincia) =>
+            this.completarProvincia(provincia)
+          )
+        })
+      )
+      .then(this.fetchLocalidades())
+      .catch((error) => console.log(error));
   }
 
-  fetchLocalidades(){
-    httpClient.get(`api/localidades/`)
-    .then((data) => this.setState({localidades: data.map(localidad => this.completarLocalidad(localidad))}))
-    .catch((error) => console.log(error));
+  fetchLocalidades() {
+    httpClient
+      .get(`api/localidades/`)
+      .then((data) =>
+        this.setState({
+          localidades: data.map((localidad) =>
+            this.completarLocalidad(localidad)
+          )
+        })
+      )
+      .catch((error) => console.log(error));
   }
 
   completarProvincia = (provincia) => {
-    provincia.pais = this.state.paises.find(pais => pais.id_pais === provincia.pais) || provincia.pais;
+    provincia.pais =
+      this.state.paises.find((pais) => pais.id_pais === provincia.pais) ||
+      provincia.pais;
 
     return provincia;
   };
 
   completarLocalidad = (localidad) => {
-    localidad.provincia = this.state.provincias.find(provincia => provincia.id_provincia === localidad.provincia) || localidad.provincia;
+    localidad.provincia =
+      this.state.provincias.find(
+        (provincia) => provincia.id_provincia === localidad.provincia
+      ) || localidad.provincia;
 
     return localidad;
   };
 
-  componentDidMount(){
+  componentDidMount() {
     //Si hay un token de autenticación en las cookies del navegador, significa que ya inició sesión un usuario, por lo que podemos obtener sus detalles desde el backend
-    if(getCookie('csrftoken')){
+    if (getCookie('csrftoken')) {
       httpClient
         .get(`api/get_detalles_usuario/`)
-        .then((data) => this.setState({usuario: data}))
+        .then((data) => this.setState({ usuario: data }))
         .catch((error) => console.log(error));
     }
 
     this.fetchInformacionGeografica();
   }
 
-    render() {
+  render() {
     return (
-      <div className="principal">
+      <div className='principal'>
         <Router>
           <Cabecera usuario={this.state.usuario} />
           <Route
             exact
             path={['/', '/elegir-accion']}
-            component={() => (
-              <ElegirAccion usuario={this.state.usuario} />
-            )}
+            component={() => <ElegirAccion usuario={this.state.usuario} />}
           />
           <Route
             exact
             path={['/reservar-puesto']}
             component={() => (
               <ReservarPuesto
-              usuario={this.state.usuario}
-              paises={this.state.paises}
-              provincias={this.state.provincias}
-              localidades={this.state.localidades}
-              id_pais_default={this.getLocalidadGeo().provincia.pais.id_pais}
-              id_provincia_default={this.getLocalidadGeo().provincia.id_provincia}
-              id_localidad_default={this.getLocalidadGeo().id_localidad} />
+                usuario={this.state.usuario}
+                paises={this.state.paises}
+                provincias={this.state.provincias}
+                localidades={this.state.localidades}
+                id_pais_default={this.getLocalidadGeo().provincia.pais.id_pais}
+                id_provincia_default={
+                  this.getLocalidadGeo().provincia.id_provincia
+                }
+                id_localidad_default={this.getLocalidadGeo().id_localidad}
+              />
             )}
           />
           <Pie />
@@ -138,7 +159,7 @@ class Index extends Component {
 }
 
 ReactDOM.render(
-  <Suspense fallback="Loading..">
+  <Suspense fallback='Loading..'>
     <Index />
   </Suspense>,
   document.getElementById('root')
